754567b2b4df853f9a976e6f9c94874c454dd544
Orrett Davis <orrett.davis@gmail.com>
2014-04-08 10:50:27 -0400

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Sevenday::Application.config.secret_key_base = 'db523f2b0ad4dada73f74323f89a1d3f106fb009c99d0870393eaae6679b9ce5887d4c271f81a14ebe5940ea2bcd4c189df9f9ef02dca5050108e692bf6c6dab'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Sevenday::Application.config.session_store :cookie_store, key: '_sevenday_session'
